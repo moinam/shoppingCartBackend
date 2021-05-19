8e70c70bd91e16d5bd23ddeb83079cf0eee6d888
Moinam Chatterjee <moinamc2007@yahoo.co.in>
2021-05-19 21:37:24 +0530

    Spring Boot Project Create
    initial commit spring boot project

---
+++ src/test/java/com/moinam/shoppingCart/ShoppingCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moinam.shoppingCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
